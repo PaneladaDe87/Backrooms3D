369f320493e13cff5ba1afe3c7572911f8c1ea34
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-03-16 23:38:23 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,17 +1,60 @@
-package;
+class Chunk {
+    var position: Vector3; // posição do chunk no mundo
+    var size: Vector3; // tamanho do chunk
+    var mesh: Mesh; // mesh do chunk
 
-import openfl.display.Sprite;
-import openfl.events.Event;
-
-class Main extends Sprite {
-    public function new() {
-        super();
-        this.addEventListener(Event.ADDED_TO_STAGE, this.onAddedToStage);
+    public function new(position: Vector3, size: Vector3) {
+        this.position = position;
+        this.size = size;
     }
 
-    private function onAddedToStage(event:Event):Void {
-        var helloWorld:TextField = new TextField();
-        helloWorld.text = "Hello, World!";
-        this.addChild(helloWorld);
+    public function generateMesh(): Void {
+        // lógica para gerar a mesh do chunk
+        // pode incluir criação de vértices, normais e índices
+    }
+}
+
+class ChunkManager {
+    var chunks: Array<Chunk>; // array que armazena todos os chunks gerados
+
+    public function new() {
+        chunks = new Array<Chunk>();
+    }
+
+    public function generateChunk(position: Vector3, size: Vector3): Chunk {
+        var chunk = new Chunk(position, size);
+        chunk.generateMesh();
+        chunks.push(chunk);
+        return chunk;
+    }
+
+    public function removeChunk(chunk: Chunk): Void {
+        var index = chunks.indexOf(chunk);
+        if (index > -1) {
+            chunks.splice(index, 1);
+        }
+    }
+
+    public function updateChunks(playerPosition: Vector3, maxDistance: Float): Void {
+        for (chunk in chunks) {
+            var distance = playerPosition.distance(chunk.position);
+            if (distance > maxDistance) {
+                removeChunk(chunk);
+            }
+        }
+    }
+}
+
+class Main {
+    static function main() {
+        var chunkManager = new ChunkManager();
+
+        // exemplo de geração de um chunk na posição (0, 0, 0) com tamanho (10, 10, 10)
+        var chunk = chunkManager.generateChunk(new Vector3(0, 0, 0), new Vector3(10, 10, 10));
+
+        // exemplo de atualização dos chunks com base na posição do jogador e distância máxima de renderização
+        var playerPosition = new Vector3(5, 0, 5);
+        var maxDistance = 50.0;
+        chunkManager.updateChunks(playerPosition, maxDistance);
     }
 }
