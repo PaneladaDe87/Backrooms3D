fb1e16b096087069e82e234921f1a4a25d7fca0d
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-03-24 20:46:46 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,4 +1,4 @@
-class Chunk {
+class PlayState {
     var position: Vector3; // posição do chunk no mundo
     var size: Vector3; // tamanho do chunk
     var mesh: Mesh; // mesh do chunk
@@ -42,19 +42,5 @@ class ChunkManager {
                 removeChunk(chunk);
             }
         }
-    }
-}
-
-class Main {
-    static function main() {
-        var chunkManager = new ChunkManager();
-
-        // exemplo de geração de um chunk na posição (0, 0, 0) com tamanho (10, 10, 10)
-        var chunk = chunkManager.generateChunk(new Vector3(0, 0, 0), new Vector3(10, 10, 10));
-
-        // exemplo de atualização dos chunks com base na posição do jogador e distância máxima de renderização
-        var playerPosition = new Vector3(5, 0, 5);
-        var maxDistance = 50.0;
-        chunkManager.updateChunks(playerPosition, maxDistance);
     }
 }
